b0e10871b9098a598866535386f1b409f3750885
papiweb <fbaez@federiconbaez.com>
2025-07-16 16:23:54 -0300

    Update vite.config.ts
    node

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,8 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    const timestamp = Date.now();
-    return {
-      base: '/gen/', // Base path para GitHub Pages
-      css: {
-        postcss: './postcss.config.js'
-      },
-      build: {
-        rollupOptions: {
-          output: {
-            entryFileNames: `assets/[name]-[hash]-${timestamp}.js`,
-            chunkFileNames: `assets/[name]-[hash]-${timestamp}.js`,
-            assetFileNames: `assets/[name]-[hash]-${timestamp}.[ext]`
-          }
-        }
-      },
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/gen/',
+})
